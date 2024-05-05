1861f9e4597247f546115c7260039590333adcbf
HAN011 <3294277501@qq.com>
2024-05-05 19:37:04 +0800

    first commit

---
+++ application/cmd/robot_cmd.h
@@ -0,0 +1,48 @@
+#ifndef ROBOT_CMD_H
+#define ROBOT_CMD_H
+//初始化 编码器值
+#define LIFT_INIT_ANGLE_RIGHT 151
+#define LIFT_INIT_ANGLE_LEFT 181
+
+#define STRETCH_1_INIT_ANGLE_LEFT 127
+#define STRETCH_1_INIT_ANGLE_RIGHT 38
+
+#define STRETCH_2_INIT_ANGLE_LEFT 73
+#define STRETCH_2_INIT_ANGLE_RIGHT 155
+
+#define HORIZONTAL_INIT 0
+//限位 编码器值
+
+#define LIFT_MAX_ANGLE_RIGHT 151
+#define LIFT_MIN_ANGLE_RIGHT -19887
+#define LIFT_MAX_ANGLE_LEFT 19885
+#define LIFT_MIN_ANGLE_LEFT 181
+
+#define STRETCH_1_MIN_ANGLE_RIGHT -24000
+#define STRETCH_1_MAX_ANGLE_RIGHT 38000
+#define STRETCH_1_MIN_ANGLE_LEFT -24000
+#define STRETCH_1_MAX_ANGLE_LEFT 38000
+
+#define STRETCH_2_MAX_ANGLE_RIGHT 14517
+#define STRETCH_2_MIN_ANGLE_RIGHT 178
+#define STRETCH_2_MAX_ANGLE_LEFT 50
+#define STRETCH_2_MIN_ANGLE_LEFT -13811
+
+#define HORIZONTAL_MIN -12016
+#define HORIZONTAL_MAX 12500
+
+/**
+ * @brief 机器人核心控制任务初始化,会被RobotInit()调用
+ * 
+ */
+void RobotCMDInit();
+
+/**
+ * @brief 机器人核心控制任务,200Hz频率运行(必须高于视觉发送频率)
+ * 
+ */
+void RobotCMDTask();
+
+void Init_Value();
+float Limit_Set(float obj,int max,int min);
+#endif // !ROBOT_CMD_H
---
+++ application/robot.c
@@ -0,0 +1,60 @@
+/**
+ * @Author: HDC h2019dc@outlook.com
+ * @Date: 2023-09-08 16:47:43
+ * @LastEditors: HDC h2019dc@outlook.com
+ * @LastEditTime: 2023-10-26 21:51:44
+ * @FilePath: \2024_Control_New_Framework_Base-dev-all\application\robot.c
+ * @Description:
+ *
+ * Copyright (c) 2023 by Alliance-EC, All Rights Reserved.
+ */
+#include "bsp_init.h"
+#include "robot.h"
+#include "robot_def.h"
+#include "robot_task.h"
+#include "buzzer.h"
+#include "chassis.h"
+#include "lift.h"
+#include "first.h"
+#include "robot_cmd.h"
+#include "second.h"
+#include "horizontal.h"
+#include "forward.h"
+
+
+void RobotInit()
+{
+    // 关闭中断,防止在初始化过程中发生中断
+    // 请不要在初始化过程中使用中断和延时函数！
+    // 若必须,则只允许使用DWT_Delay()
+    __disable_irq();
+
+    BSPInit();
+  // buzzer_one_note(Do_freq, 0.1);
+    RobotCMDInit();
+  //BuzzerPlay(RoboMaster_You);
+   // buzzer_one_note(La_freq, 1);
+ ChassisInit();                  
+   // buzzer_one_note(So_freq, 0.1f);
+    Lift_Init();
+    First_Stretch_Init();
+    Second_Stretch_Init();
+      //  buzzer_one_note(Mi_freq, 1);
+
+  Horizontal_Init();
+    Forward_Init();
+    // 初始化完成,开启中断
+    __enable_irq();
+}
+
+void RobotTask()
+{
+    
+    RobotCMDTask();
+    ChassisTask();
+    Lift_Task();
+   First_Stretch_Task();
+    Second_Stretch_Task();
+    Horizontal_Task();
+    Forward_Task();
+}
